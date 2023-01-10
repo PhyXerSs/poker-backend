c24d933431bc44a7bf6a43108139edb57f1c98d1
PhyXerSs <kuntaice28@gmail.com>
2023-01-11 01:06:36 +0700

    vercel deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,6 @@ import firestore from './utils/firebase';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
